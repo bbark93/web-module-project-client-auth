de1154dff34490fa03acfb545ec37044d6ba290d
Brandon Barksdale <bbark93@gmail.com>
2024-01-17 18:44:15 -0500

    task 2: Build the login component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "./App.css";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
-const Login = () => {
-  return <h2>Login</h2>;
-};
-const FriendsList = () => {
-  return <h2>FriendsList</h2>;
-};
-const AddFriend = () => {
-  return <h2>AddFriends</h2>;
-};
-
-
+import Login from "./Components/Login";
+import FriendsList from "./Components/FriendsList";
+import AddFriend from "./Components/AddFriend";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <h2>Friends Database</h2>
+        <Link className="link" to='login'>Login</Link>
+        <Link className="link" to='friends'>Friends List</Link>
+        <Link className="link" to='friends/add'>Add Friends</Link>
+        <Link className="link" to='login'>Logout</Link>
+      </header>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
---
+++ src/Components/FriendsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FriendsList = () => {
+  return (
+    <div>
+      <h2>FriendsList</h2>
+      <ul>
+        <li>Name-Age-Email</li>
+        <li>Name-Age-Email</li>
+        <li>Name-Age-Email</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FriendsList;
